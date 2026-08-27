2ddfd6a4f24c52d5b349c0557000b78fad415bbd
devmetwaly-000 <dev.metwaly@gmail.com>
2026-08-27 21:25:38 +0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCD37YFQpy-zPZm8ReKKdrYj9f7QHAXTLo",
   authDomain: "new-highclass.firebaseapp.com",
+  databaseURL: "https://new-highclass-default-rtdb.firebaseio.com",
   projectId: "new-highclass",
   storageBucket: "new-highclass.firebasestorage.app",
   messagingSenderId: "16681653813",
-  appId: "1:16681653813:web:12e0e89e4b22833ca1837c"
+  appId: "1:16681653813:web:43d88fcaaa8781c8a1837c"
 };
 
 
