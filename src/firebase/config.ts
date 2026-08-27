f60d84e5f0a9f64b6ad6372968a706f406f40794
Omar Essam <omar.essam@gomlamarket.com>
2026-08-27 22:20:12 +0300

    update firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
-const firebaseConfig = {
-  "apiKey": "AIzaSyAhgT44bgwIVtFM2Ex91a93B4TezJn-IyI",
-  "authDomain": "v-highclass.firebaseapp.com",
-  "databaseURL": "https://v-highclass-default-rtdb.firebaseio.com",
-  "projectId": "v-highclass",
-  "storageBucket": "v-highclass.firebasestorage.app",
-  "messagingSenderId": "469425986623",
-  "appId": "1:469425986623:web:3770093155a71b035f6c8a",
-  "measurementId": "G-XXHC38PVRQ"
+export const firebaseConfig = {
+  "apiKey": "AIzaSyCD37YFQpy-zPZm8ReKKdrYj9f7QHAXTLo",
+  "authDomain": "new-highclass.firebaseapp.com",
+  "databaseURL": "https://new-highclass-default-rtdb.firebaseio.com",
+  "projectId": "new-highclass",
+  "storageBucket": "new-highclass.firebasestorage.app",
+  "messagingSenderId": "16681653813",
+  "appId": "1:16681653813:web:43d88fcaaa8781c8a1837c"
 };
