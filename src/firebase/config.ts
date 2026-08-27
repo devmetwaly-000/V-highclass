f70fde4e8e7be8540299285e067c37c42cf941e9
devmetwaly-000 <dev.metwaly@gmail.com>
2026-08-27 20:04:42 +0300

    metometwaly

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,21 @@
-export const firebaseConfig = {
-  "apiKey": "AIzaSyAhgT44bgwIVtFM2Ex91a93B4TezJn-IyI",
-  "authDomain": "v-highclass.firebaseapp.com",
-  "databaseURL": "https://v-highclass-default-rtdb.firebaseio.com",
-  "projectId": "v-highclass",
-  "storageBucket": "v-highclass.firebasestorage.app",
-  "messagingSenderId": "469425986623",
-  "appId": "1:469425986623:web:3770093155a71b035f6c8a",
-  "measurementId": "G-XXHC38PVRQ"
+export 
+const firebaseConfig = {
+  apiKey: "AIzaSyCD37YFQpy-zPZm8ReKKdrYj9f7QHAXTLo",
+  authDomain: "new-highclass.firebaseapp.com",
+  projectId: "new-highclass",
+  storageBucket: "new-highclass.firebasestorage.app",
+  messagingSenderId: "16681653813",
+  appId: "1:16681653813:web:12e0e89e4b22833ca1837c"
 };
+
+
+// const firebaseConfig = {
+//   "apiKey": "AIzaSyAhgT44bgwIVtFM2Ex91a93B4TezJn-IyI",
+//   "authDomain": "v-highclass.firebaseapp.com",
+//   "databaseURL": "https://v-highclass-default-rtdb.firebaseio.com",
+//   "projectId": "v-highclass",
+//   "storageBucket": "v-highclass.firebasestorage.app",
+//   "messagingSenderId": "469425986623",
+//   "appId": "1:469425986623:web:3770093155a71b035f6c8a",
+//   "measurementId": "G-XXHC38PVRQ"
+// };
