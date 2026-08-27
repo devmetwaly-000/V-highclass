e72f018070019d677d7b2299236e1cbdc3a1c566
devmetwaly-000 <dev.metwaly@gmail.com>
2026-08-27 21:47:40 +0300

    New Stings

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCD37YFQpy-zPZm8ReKKdrYj9f7QHAXTLo",
-  authDomain: "new-highclass.firebaseapp.com",
-  databaseURL: "https://new-highclass-default-rtdb.firebaseio.com",
-  projectId: "new-highclass",
-  storageBucket: "new-highclass.firebasestorage.app",
-  messagingSenderId: "16681653813",
-  appId: "1:16681653813:web:43d88fcaaa8781c8a1837c"
+  "apiKey": "AIzaSyCD37YFQpy-zPZm8ReKKdrYj9f7QHAXTLo",
+  "authDomain": "new-highclass.firebaseapp.com",
+  "databaseURL": "https://new-highclass-default-rtdb.firebaseio.com",
+  "projectId": "new-highclass",
+  "storageBucket": "new-highclass.firebasestorage.app",
+  "messagingSenderId": "16681653813",
+  "appId": "1:16681653813:web:43d88fcaaa8781c8a1837c"
 };
 
 
